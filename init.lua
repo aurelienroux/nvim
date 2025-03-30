513986e481f23afadfd7544bce88a843a4d450e0
Aurélien Roux <aureroux13@gmail.com>
2025-03-30 18:28:15 +0200

    add yanking highlight

--- init.lua
+++ init.lua
@@ -1,9 +1,22 @@
--- -- Set <space> as the leader key, see `:help mapleader`
--- -- Note: must happen before plugins are loaded (otherwise wrong leader will be used)
+-- Set <space> as the leader key, see `:help mapleader`
+-- Note: must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
 vim.cmd.colorscheme "onedark"
